670bc66d84f7009ab803060115af3018990d577c
Max Aalto <maalto@maalto-ltm.internal.salesforce.com>
2016-02-27 12:27:34 -0800

    routes for Map API calls

--- maps.js
+++ maps.js
@@ -1,3 +1,4 @@
+var exports = module.exports = {};
 var GoogleMaps = require('googlemaps');
 var config = require('./config.json');
 var gm_API_KEY = config.maps_api_key;
@@ -6,69 +7,53 @@ var publicConfig = {
   stagger_time:       1000,
   encode_polylines:   false,
   secure:             true,
-  // proxy:              'http://localhost:3000'
 };
 var gmAPI = new GoogleMaps(publicConfig);
-//
-var params = {
-  location: '51.507868,-0.087689',
-  size: '1200x1600',
-  heading: 108.4,
-  pitch: 7,
-  fov: 40
-};
-// var result = gmAPI.streetView(params);
-// console.log(result);
-gmAPI.directions({origin:'31.470656,74.412929', destination:'31.470789,74.408619'},
-  function(err, data) {
-  console.log(JSON.stringify(data));
-});
 
-var params = {
-  center: '444 W Main St Lock Haven PA',
-  zoom: 15,
-  size: '500x400',
-  maptype: 'roadmap',
-  markers: [
-    {
-      location: '300 W Main St Lock Haven, PA',
-      label   : 'A',
-      color   : 'green',
-      shadow  : true
-    },
-    {
-      location: '444 W Main St Lock Haven, PA',
-      icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_icon&chld=cafe%7C996600'
-    }
-  ],
-  style: [
-    {
-      feature: 'road',
-      element: 'all',
-      rules: {
-        hue: '0x00ff00'
+exports.getDirections = function(origin, destination) {
+  gmAPI.directions({origin:origin.long+","+origin.lat, destination:destination.long+','+destination.lat},
+    function(err, data) {
+    console.log(JSON.stringify(data));
+  });
+}
+
+exports.getStaticMap = function(origin, destination) {
+  var params = {
+    center: origin.long+","+origin.lat
+    zoom: 15,
+    size: '500x400',
+    maptype: 'roadmap',
+    markers: [
+      {
+        location: destination.long+','+destination.lat,
+        color   : 'green',
+        shadow  : true
+      },
+      {
+        location: origin.long+","+origin.lat,
       }
-    }
-  ],
-  path: [
-    {
-      color: '0x0000ff',
-      weight: '5',
-      points: [
-        '41.139817,-77.454439',
-        '41.138621,-77.451596'
-      ]
-    }
-  ]
-};
-gmAPI.staticMap(params); // return static map URL
-gmAPI.staticMap(params, function(err, binaryImage) {
-  console.log(binaryImage);
-});
-//
-// var gm = require('googlemaps');
-// var util = require('util');
-//
-// gm.config('key', gm_API_KEY);
-// gm.directions('31.470656,74.412929', '31.470789,74.408619' ,
-// function(err, data){util.puts(JSON.stringify(data));});
+    ],
+    style: [
+      {
+        feature: 'road',
+        element: 'all',
+        rules: {
+          hue: '0x00ff00'
+        }
+      }
+    ],
+    path: [
+      {
+        color: '0x0000ff',
+        weight: '5',
+        points: [
+          '41.139817,-77.454439',
+          '41.138621,-77.451596'
+        ]
+      }
+    ]
+  };
+  gmAPI.staticMap(params, function(err, binaryImage) {
+    console.log(binaryImage);
+  });
+}
